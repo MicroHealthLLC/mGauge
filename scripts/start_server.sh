1af81b3d743bcca1e790f1e7ce4d64b0a2410d4a
Zach Egner <zach.egner@microhealthllc.com>
2023-08-16 12:01:42 -0400

    modifies deployment appspec

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Navigate to your application's directory
-# cd ..
+cd /var/www/mGauge
 
 # Start your Node.js server using Express
 # Replace 'router.js' with the appropriate entry point file
-npm run start &
+npm run start
 
 # Save the process ID of the server
 echo $! > server.pid
